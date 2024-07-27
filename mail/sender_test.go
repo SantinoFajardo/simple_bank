ac278a714abc6a8bd5604862d4397015c9e8ba55
FajardoSatino <santino@sphereone.xyz>
2024-07-27 11:47:26 -0300

    avoiding send email on ci/cd test running

--- mail/sender_test.go
+++ mail/sender_test.go
@@ -9,6 +9,9 @@ import (
 )
 
 func TestSendEmailWithGmail(t *testing.T) {
+	if testing.Short() {
+		t.Skip()
+	}
 	config, err := util.LoadConfig("..")
 	require.NoError(t, err)
 	fmt.Printf("NAME,ADDRESS,PASSWORD = %s, %s, %s", config.EmailSenderName, config.EmailSenderAddress, config.EmailSenderPassword)
